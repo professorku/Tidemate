2fa88b38c95c89eb62d11eabb29259445192327c
jso078 <jso078@uit.no>
2026-05-01 12:46:02 +0200

    Yes

--- frontend/src/features/hostBookings/components/HostBookingCard.jsx
+++ frontend/src/features/hostBookings/components/HostBookingCard.jsx
@@ -129,4 +129,4 @@ export default function BookingCard({
       </div>
     </article>
   )
-}
+} 
--- frontend/src/features/hostBookings/components/bookingCard/BookingCardFooter.jsx
+++ frontend/src/features/hostBookings/components/bookingCard/BookingCardFooter.jsx
@@ -51,4 +51,4 @@ export default function BookingCardFooter({
       ) : null}
     </div>
   )
-}
+} 
