a3570fdf1e3b70a59fccf2edf854399b0b4cbb3a
jso078 <jso078@uit.no>
2026-05-02 02:36:00 +0200

    Remove unused files and cleanup dead code

--- frontend/src/features/profile/components/ProfileBoatCard.jsx
+++
@@ -1,5 +0,0 @@
-import BoatPreviewCard from '../../../components/ui/BoatPreviewCard'
-
-export default function BoatCard({ boat }) {
-  return <BoatPreviewCard boat={boat} badge="Listed" />
-}
--- frontend/src/features/publicProfile/utils/formatMemberSince.js
+++
@@ -1,5 +0,0 @@
-import { formatMemberSince as baseFormatMemberSince } from '../../../utils/format/date'
-
-export default function formatMemberSince(value) {
-  return baseFormatMemberSince(value)
-}
